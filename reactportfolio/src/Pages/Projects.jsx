904c5c7c816796d1fdd27886b0dee95af9ee7ab7
rosiegalezia <rosegalezia@gmail.com>
2024-01-29 14:33:56 +0000

    trying to redeploy

--- reactportfolio/src/Pages/Projects.jsx
+++ reactportfolio/src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ import '../Pages/Styles.css'
 
 function Projects() {
     return <div className="background min-vh-100 pb-5">
-        <h2 className="p-5 pb-3">Take a look at my previous projects:</h2>
+        <h2 className="p-5 pb-3">Take a look at some of my previous projects:</h2>
         <div className="flex-wrap d-flex m-4 p-4">
             {projects.map((project) => {
                 return <Project key={project.id} title={project.title} image={project.image} description={project.description} tools={project.tools} deployed={project.deployed} github={project.github} />;
